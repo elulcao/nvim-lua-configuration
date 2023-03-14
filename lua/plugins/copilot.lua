16af35904d416f7c8f9f7a17c22283f28862e922
Daniel Carvallo <elulcao@icloud.com>
2023-03-13 18:47:15 -0600

    Update Copilot

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -6,8 +6,27 @@ if not setup then
 end
 
 copilot.setup({
-	copilot_enable = 1,
-	copilot_filetypes = {
+	-- copilot_enable = 1,
+	-- copilot_filetypes = {
+	-- ["*"] = { "v:true" },
+	-- },
+	cmp = {
+		enabled = true,
+		method = "getCompletionsCycling",
+	},
+	panel = {
+		enabled = true,
+	},
+	server_opts_overrides = {
+		-- trace = "verbose",
+		settings = {
+			advanced = {
+				-- listCount = 10, -- #completions for panel
+				inlineSuggestCount = 3, -- #completions for getCompletions
+			},
+		},
+	},
+	filetypes = {
 		["*"] = { "v:true" },
 	},
 })
